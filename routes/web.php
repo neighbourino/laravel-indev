98c8b4c807065e976815b4828902cefac4d4e231
Janus Helkjær <jh@mindmill.dk>
2024-03-14 12:55:57 +0100

    removed daemon specification

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('/restart-daemon', function () {
 
 
  
-    $result = Process::run('sudo -S supervisorctl restart daemon-ID:804179');
+    $result = Process::run('sudo -S supervisorctl restart daemon-ID:*');
 
     dd($result,  $result->output());
  
